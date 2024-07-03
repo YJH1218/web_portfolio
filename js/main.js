999dd87cfe37023b74685d3a93d18be3fcb654c0
yjh1218 <roo1218@naver.com>
2024-07-03 16:11:17 +0900

    디자인 조금 수정 - 마퀴, 메인 컨텐츠 버튼, 디자인 타이틀 여백

--- js/main.js
+++ js/main.js
@@ -2,34 +2,6 @@ $(document).ready(function(){
     
     gsap.registerPlugin(ScrollTrigger);
    
-    gsap.to(".icon_01", {
-        scrollTrigger: {
-            trigger: "footer",
-            start: "0% 80%",
-            toggleActions: "restart pause resume reverse"
-        },
-        opacity: 1,
-        y: -40,
-    })
-    gsap.to(".icon_02", {
-        scrollTrigger: {
-            trigger: "footer",
-            start: "0% 80%",
-            toggleActions: "restart pause resume reverse"
-        },
-        opacity: 1,
-        y: -40,
-        delay: 0.2,
-    })
-    gsap.to(".icon_03", {
-        scrollTrigger: {
-            trigger: "footer",
-            start: "0% 80%",
-            toggleActions: "restart pause resume reverse"
-        },
-        opacity: 1,
-        y: -40,
-        delay: 0.4,
-    })
+
 
 });
