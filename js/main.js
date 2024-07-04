e83a96e57fe14f62808b17f5d59d4f5ce710d442
yjh1218 <roo1218@naver.com>
2024-07-04 11:57:26 +0900

    모바일 반응형 틀 완성

--- js/main.js
+++ js/main.js
@@ -3,10 +3,6 @@ $(document).ready(function(){
     gsap.registerPlugin(ScrollTrigger);
    
 
-    $('.mob_hamburger').click(function(){
-        $('.mob_menu, .X_mark, .mob_hamburger').addClass("active");
-    });
-    $('.mob_menu .bg, .mob_menu ul li, .X_mark').click(function(){
-        $('.mob_menu, .X_mark, .mob_hamburger').removeClass("active");
-    });
+
+
 });
